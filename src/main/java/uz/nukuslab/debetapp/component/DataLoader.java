3ed9610d3e44b91aaca0f56b2473def4ef932a30
sayaubay <saylaubaybekmurzaev@gmail.com>
2022-09-04 22:30:50 +0500

    #7

--- src/main/java/uz/nukuslab/debetapp/component/DataLoader.java
+++ src/main/java/uz/nukuslab/debetapp/component/DataLoader.java
@@ -47,6 +47,8 @@ public class DataLoader implements CommandLineRunner {
                     RoleName.USER
             ));
 
+
+//            klklkk
 //            Company nukuslab_llc = companyRepository.save(new Company(
 //                    "Nukuslab",
 //                    true
