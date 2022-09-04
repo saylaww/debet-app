b51294fe797008bc022baf3e4f01a67341d6b430
sayaubay <saylaubaybekmurzaev@gmail.com>
2022-09-04 22:52:21 +0500

    #10

--- src/main/java/uz/nukuslab/debetapp/config/SecurityConfig.java
+++ src/main/java/uz/nukuslab/debetapp/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().disable()
+                .cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeRequests()
